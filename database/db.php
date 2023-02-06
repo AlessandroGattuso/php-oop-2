b179692b3fe886420363537e0da73e093390d7b9
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-02-06 21:26:10 +0100

    added some items

--- database/db.php
+++ database/db.php
@@ -8,9 +8,11 @@
     new Food('Mangime per uccelli', 'bird', 20),
     new Food('Mangime per pesci', 'fish', 12),
     new Game('Gomitolo per gatti', 'cat', 30),
+    new Game('Castello per pesci', 'fish', 30),
     new Game('Bastone per cani', 'dog', 25),
     new Cage('Gabbia per cani', 'dog', 100),
-    new Cage('Gabbia per gatti', 'cat', 150)
+    new Cage('Gabbia per gatti', 'cat', 150),
+    new Cage('Gabbia per uccelli', 'bird', 75),
   ];
 
 ?>
